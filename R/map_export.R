0fd356c763296bf3bdde246540fa5d007d2fffd1
dennisvm <d.van.muijen@rijkzwaan.nl>
2017-10-10 12:52:49 +0200

    cran check pass

--- R/map_export.R
+++ R/map_export.R
@@ -3,21 +3,38 @@
 #'
 #' Writes the consensus map to the JoinMap ".map" format
 #' @param MF.obj The mapfuser object with filled results slot
-#' @param file A connection or a string giving the file name to write the map
-#' to.
+#' @param file Path to the output file
 #' @author Dennis van Muijen
 #' @importFrom utils write.table
 #' @examples
 #' \dontshow{
-#' tmp <- tempfile()
-#' MF.obj <- list()
-#' class(MF.obj) <- "mapfuser"
-#' MF.obj$result$consensus_map <- data.frame(Marker = LETTERS[1:5], LG = c(1,1,1,2,2), Position = 1:5)
-#' map_export(MF.obj, file = tmp)
-#' file.remove(paste0(tmp,".map"))
+#' fpath <- system.file("extdata", package="mapfuser")
+#' maps <- list.files(fpath, pattern = "-1", full.names = TRUE)
+#' MF.obj <- read_maps(mapfiles = maps, sep = ",", header = TRUE, type = "delim")
+#' MF.obj <- map_qc(MF.obj)
+#' MF.obj$config$chr <- "1"
+#' MF.obj <- LPmerge_par(MF.obj = MF.obj, n.cores = 1,
+#' max.interval = 1, max.int_sel = "auto", weights = NULL)
+#' file <- paste(tempdir(), "/consensus.map", sep="")
+#' map_export(MF.obj, file)
 #' }
 #' \dontrun{
-#' map_export(MF.obj = MF.obj, file = "consensus")
+#' ## Read maps
+#' fpath <- system.file("extdata", package="mapfuser")
+#' maps <- list.files(fpath, pattern = "Col", full.names = TRUE)
+#' MF.obj <- read_maps(mapfiles = maps, sep = ",", header = TRUE,
+#' mapweights = rep(1,7), type = "delim")
+#' 
+#' ## Run map_qc
+#' MF.obj <- map_qc(MF.obj, anchors = 3)
+#' 
+#' ## Construct consensus map
+#' MF.obj <- LPmerge_par(MF.obj = MF.obj, n.cores = 2,
+#' max.interval = 1:3, max.int_sel = "auto", weights = NULL)
+#' 
+#' ## Export to JoinMap format
+#' file <- paste(tempdir(), "/consensus.map", sep="")
+#' map_export(MF.obj, file)
 #' }
 #' @export
 
@@ -32,7 +49,6 @@ map_export <- function(MF.obj, file = NULL) {
     "; ngrp = ", length(unique(map$LG)),
     ", nloc = ", nrow(map), sep = ""
   ), stringsAsFactors = F)
-  mapvec
   for (i in seq_along(grp)) {
     mapvec <- rbind.data.frame(mapvec, paste(""))
     mapvec <- rbind.data.frame(mapvec, paste("group ", grp[i]), stringsAsFactors = F, sep = "")
@@ -40,9 +56,9 @@ map_export <- function(MF.obj, file = NULL) {
       mapvec <- rbind.data.frame(mapvec, paste(map$Marker[j], map$Position[j], " ;"), stringsAsFactors = F)
   }
   if (!is.null(file)){
-    write.table(mapvec, paste0(file,".map"), sep = "", quote = FALSE, col.names = FALSE, row.names = FALSE)
+    write.table(mapvec, file, sep = "", quote = FALSE, col.names = FALSE, row.names = FALSE)
   } else {
-    cat("Filename not supplied..")
+    cat("Filename not supplied.. returning genetic map in JoinMap format")
     return(mapvec)
   }
 }
